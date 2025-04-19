9e7dc9ea0e850fd1e36de2f12c583e0195567994
Adrian <AdrianLTR>
2025-04-18 20:44:06 -0400

    Solución para página en blanco: ajustar rutas relativas y configurar redirección SPA

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './', // Esto permite que los enlaces relativos funcionen correctamente en GitHub Pages
 })
